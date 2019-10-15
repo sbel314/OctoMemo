a73689909a5aa1853a8ebaa783d7ddaf852e33fc
leonardofurtado <srleonardofurtado@gmail.com>
2019-10-15 11:19:56 -0300

    adding docstrings

--- scripts/user.py
+++ scripts/user.py
@@ -1,5 +1,4 @@
 from github import Github
-import getpass
 
 
 class User:
@@ -9,22 +8,35 @@ class User:
         self.auth = Github(login, password)
 
     def create_repo(self):
+        """Method to create a repository for notes."""
         self.auth.get_user().create_repo("octomemo_" + self._login, private=True)
 
     def list_all_notes(self):
+        """Method to list all notes in a github repository."""
         repo = self.auth.get_user().get_repo("octomemo_" + self._login)
         print("Avaiable notes:")
         for file in repo.get_dir_contents(""):
             print(file.name)
 
     def create_note(self, name, message="none", content=""):
+        """Method to create a note in a github repository.
+
+        :param str name: Note name
+        :param str message: Commit message
+        :param str content: Note content
+        """
         repo = self.auth.get_user().get_repo("octomemo_" + self._login)
         repo.create_file(name, message, content)
 
-    def delete_note(self, nome, message="deleting file"):
-        repo = self.auth.get_user().get_repo("octomemo_" + self._login)
-        sha = repo.get_contents(nome).sha
-        repo.delete_file(nome, message, sha)
+    def delete_note(self, name, message="deleting file"):
+        """Method to delete a note in a github repository.
 
-    def edit_note(self, path, message="Removing file"):
+        :param str name: Name of note to be deleted
+        :param str message: Commit message
+        """
+        repo = self.auth.get_user().get_repo("octomemo_" + self._login)
+        sha = repo.get_contents(name).sha
+        repo.delete_file(name, message, sha)
+
+    def edit_note(self):
         pass
